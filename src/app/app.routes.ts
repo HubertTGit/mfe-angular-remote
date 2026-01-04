21c902beffcafa4a594f4817706764626df6225e
HuberttGit <hubert.talib@gmail.com>
2026-01-04 20:11:11 +0100

    feat: Add Home and Dashboard components with routing and navigation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then((m) => m.Home),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard').then((m) => m.Dashboard),
+  },
+];
