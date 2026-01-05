a55513abfc7903fca3b580216dba759c457b97b9
HuberttGit <hubert.talib@gmail.com>
2026-01-05 16:13:25 +0100

    feat: Register LoginUi and CommentsInput components as custom elements.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
+import { createCustomElement } from '@angular/elements';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { LoginUi } from './app/ui/login-ui/login-ui';
+import { CommentsInput } from './app/ui/comments-input/comments-input';
 
 bootstrapApplication(App, appConfig)
+  .then((appRef) => {
+    const loginUiElement = createCustomElement(LoginUi, {
+      injector: appRef.injector,
+    });
+    customElements.define('login-ui', loginUiElement);
+
+    const commentsInputElement = createCustomElement(CommentsInput, {
+      injector: appRef.injector,
+    });
+    customElements.define('comments-input', commentsInputElement);
+  })
   .catch((err) => console.error(err));
