09e4bc50f9ce9c7dc1d83d20c8d3c9623f1bf372
HuberttGit <hubert.talib@gmail.com>
2026-01-10 20:13:13 +0100

    add authguard and redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./login/login').then((m) => m.Login),
+    canActivate: [guestGuard],
   },
   {
     path: 'dashboard',
     loadComponent: () => import('./dashboard/dashboard').then((m) => m.Dashboard),
+    canActivate: [authGuard],
   },
 ];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { inject } from '@angular/core';
+import { Router, type CanActivateFn } from '@angular/router';
+import { map, take } from 'rxjs';
+import { LoginFacade } from '../login/login.facade';
+
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const loginFacade = inject(LoginFacade);
+
+  return loginFacade.userState$().pipe(
+    take(1),
+    map((user) => {
+      if (user) {
+        return true;
+      }
+      return router.createUrlTree(['/']);
+    })
+  );
+};
+
+export const guestGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const loginFacade = inject(LoginFacade);
+
+  return loginFacade.userState$().pipe(
+    take(1),
+    map((user) => {
+      if (!user) {
+        return true;
+      }
+      return router.createUrlTree(['/dashboard']);
+    })
+  );
+};
