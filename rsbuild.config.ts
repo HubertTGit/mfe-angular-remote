a8aa6cd183257f666cd19adf14ccf341701cc813
HuberttGit <hubert.talib@gmail.com>
2026-01-12 21:34:36 +0100

    spit theme and tailwind

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -23,6 +23,7 @@ export default createConfig({
         return config;
       },
     },
+
     plugins: [
       pluginSass(),
       pluginModuleFederation({
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import './styles.css';
+import '@styles/styles.css';
 
 bootstrapApplication(App, appConfig);
